1c37998a4d99b9fff4541f63186adf6e70c6e50f
lacravate <david.elbaz@af83.com>
2011-10-31 02:19:32 +0100

    more tests; on features; on methods; still on the run

--- test/test.js
+++ test/test.js
@@ -70,9 +70,10 @@ test("Setup with a more complex html structure and options, local update value a
 
 module('Methods');
 
-function setupMethod(){
-    loadFixtures('minimum_valid_form.html');
-    return $.in_da_house.houses[$('.edit_in_da_house').in_da_house().attr('class')];
+function setupMethod(options, fixture){
+    fixture = fixture || 'minimum_valid_form.html';
+    loadFixtures(fixture);
+    return $.in_da_house.houses[$('.edit_in_da_house').in_da_house(options).attr('class')];
 }
 
 test("initialize method", function() {
@@ -85,53 +86,86 @@ test("initialize method", function() {
     ok(da_house._editable);
     // TODO test events and callbacks
 });
-test("updateEditableAndSwitch method pending", function() {
-  ok(true, "Test pending");
+test("updateEditableAndSwitch method", function() {
+    var da_house = setupMethod();
+    da_house._resource[da_house._attribute] = 2;
+
+    da_house.switchEditor();
+    da_house.updateEditableAndSwitch();
+
+    equals(da_house._editable.attr('style'), 'display: inline;', "Editable elements should be visible");
+    equals(da_house._editable.html(), '2', 'Editable inner HTML is updated');
 });
-test("updateResource method pending", function() {
-  ok(true, "Test pending");
+test("updateResource method", function() {
+    var da_house = setupMethod({ remoteUpdate: false });
+    ok(!da_house._resource[da_house._attribute], "input has no set value");
+    da_house._input.val('2');
+    da_house.updateResource();
+    equals(da_house._resource[da_house._attribute], '2', "input has now value");
 });
-test("setResource method pending", function() {
-  ok(true, "Test pending");
+test("setResource method", function() {
+    // value update is covered by "updateResource method" test. Nevertheless
+    var da_house = setupMethod({ remoteUpdate: false });
+    ok(!da_house._resource[da_house._attribute], "input has no set value");
+    da_house.setResource({ bar_baz: '2'});
+    equals(da_house._resource[da_house._attribute], '2', "input has now value");
+    // TODO : test event triggering
 });
 test("remoteUpdate method pending", function() {
-  ok(true, "Test pending");
+    ok(true, "Test pending");
 });
 test("localUpdate method pending", function() {
-  ok(true, "Test pending");
+    var da_house = setupMethod({ remoteUpdate: false });
+    // value update is covered by "updateResource method" and "setResource method" tests.
+    // Will see later if there's anything specific to be done here.
+    ok(true, "Test pending");
 });
-test("setAttribute pending", function() {
-  ok(true, "Test pending");
+test("setAttribute method", function() {
+    var da_house_one = setupMethod({ remoteUpdate: false });
+    equals(da_house_one._attribute, 'bar_baz', "We found bar_baz as the attribute");
+    ok(da_house_one.el.hasClass('bar_baz'), "bar_baz class was set on container");
+
+    var da_house_two = setupMethod({ remoteUpdate: false }, 'minimum_valid_form_with_other_attribute.html');
+    equals(da_house_two._attribute, 'other', "We found 'other' as the attribute");
+    ok(da_house_two.el.hasClass('other'), "'other' class was set on container");
+
+    var da_house_three = setupMethod({ remoteUpdate: false }, 'minimum_valid_form_with_data.html');
+    equals(da_house_three._attribute, 'foo_bar_baz', "We found foo_bar_baz as the attribute");
+    ok(da_house_three.el.hasClass('foo_bar_baz'), "foo_bar_baz class was set on container");
 });
-test("setEditable method pending", function() {
-  ok(true, "Test pending");
+test("setEditable method", function() {
+    var da_house_one = setupMethod({ remoteUpdate: false });
+    equals(da_house_one._editable.html(), da_house_one.setDefaultEditableText(), "We found the right editable");
+
+    var da_house_two = setupMethod({ remoteUpdate: false }, 'minimum_valid_form_with_editable.html');
+    equals(da_house_two._editable.html(), $('#set_editable').html(), "We found the right editable");
 });
 test("setEditor method pending", function() {
-  ok(true, "Test pending");
+    ok(true, "Test pending");
 });
 test("setDefautSubmit method pending", function() {
-  ok(true, "Test pending");
+    ok(true, "Test pending");
 });
 test("setDefaultEditable method pending", function() {
-  ok(true, "Test pending");
+    ok(true, "Test pending");
 });
 test("setDefaultEditableText method pending", function() {
-  ok(true, "Test pending");
+    ok(true, "Test pending");
 });
 test("getUrl method pending", function() {
-  ok(true, "Test pending");
+    ok(true, "Test pending");
 });
 test("switchOnAndOff method pending", function() {
-  ok(true, "Test pending");
+    ok(true, "Test pending");
 });
 test("switchEditor method pending", function() {
-  ok(true, "Test pending");
+    ok(true, "Test pending");
 });
 test("getForm method pending", function() {
-  ok(true, "Test pending");
+    ok(true, "Test pending");
 });
 test("ajaxDefaults method pending", function() {
-  ok(true, "Test pending");
+    ok(true, "Test pending");
 });
 
 })();
